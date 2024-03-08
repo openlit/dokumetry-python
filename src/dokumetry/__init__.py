58f303a09c4739aa054cfadf4b430699a124f0cd
patcher99 <patcher99@dokulabs.com>
2024-03-08 19:01:49 +0530

    change if conditional in init

--- src/dokumetry/__init__.py
+++ src/dokumetry/__init__.py
@@ -43,7 +43,7 @@ def init(llm, doku_url, api_key, environment="default", application_name="defaul
     DokuConfig.skip_resp = skip_resp
 
     # pylint: disable=no-else-return, line-too-long
-    if hasattr(llm, 'chat') and callable(llm.chat.completions.create) and ('.openai.azure.com/' not in str(llm.base_url)):
+    if hasattr(llm, 'moderations') and callable(llm.chat.completions.create) and ('.openai.azure.com/' not in str(llm.base_url)):
         init_openai(llm, doku_url, api_key, environment, application_name, skip_resp)
         return
     # pylint: disable=no-else-return
