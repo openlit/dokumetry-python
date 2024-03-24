8c6a2ac57c3a3449d424cb643db97cb2e0b284a3
patcher99 <patcher99@dokulabs.com>
2024-03-24 14:09:27 +0530

    add max tokens to mistral tests

--- tests/test_mistral.py
+++ tests/test_mistral.py
@@ -48,6 +48,7 @@ def test_chat():
     message = client.chat(
         model="mistral-large-latest",
         messages=messages,
+        max_tokens=10,
     )
     assert message.object == 'chat.completion'
 
